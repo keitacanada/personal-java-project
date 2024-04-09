7d1003780cb5f02c8175a829f08d59c6d9cf58f7
keitacanada <keiy@Keis-iMac.local>
2024-04-08 21:58:09 -0700

    added SecurityFilterChain

---
+++ src/main/java/com/keita/restapi/security/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.keita.restapi.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrfConfig -> csrfConfig.disable())
+                .authorizeHttpRequests( authConfig -> {
+                    authConfig.anyRequest().authenticated();
+                });
+
+
+        return http.build();
+    }
+}
